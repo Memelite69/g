041f64a6e4f5e62e82a10a52414df2f236bb2c6a
Mr-funkinguy <89234368+Mr-funkinguy@users.noreply.github.com>
2023-04-17 18:05:09 -0700

    Add files via upload

--- doom/main.js
+++ doom/main.js
@@ -90,7 +90,7 @@ var importObject = {
     }
 };
 
-WebAssembly.instantiateStreaming(fetch('doom.wasm'), importObject)
+WebAssembly.instantiateStreaming(fetch('https://github.com/Mr-funkinguy/wasmStuff/raw/main/doom/doom.wasm'), importObject)
     .then(obj => {
 
     /*Initialize Doom*/
